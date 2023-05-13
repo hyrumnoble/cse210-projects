038c3a1e856811a945ffe52193035a90677ff1a8
hyrumnoble <hyrumnoble3@gmail.com>
2023-05-13 07:55:23 -0700

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Please enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Your name is {1}, {0}, {1}", firstName, lastName);
     }
 }
