b876fa00f9bb50f62a1753c4c482af2fd663be5e
hyrumnoble <hyrumnoble3@gmail.com>
2023-05-13 07:59:55 -0700

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace NumberListProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep4 World!");
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == 0)
+                {
+                    break;
+                }
+
+                numbers.Add(num);
+            }
+
+            int sum = 0;
+            int max = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                sum += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            double avg = (double)sum / numbers.Count;
+
+            Console.WriteLine("The sum is: " + sum);
+            Console.WriteLine("The average is: " + avg);
+            Console.WriteLine("The largest number is: " + max);
+        }
     }
 }
