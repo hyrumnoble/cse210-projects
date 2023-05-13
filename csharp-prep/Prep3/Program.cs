6b29a4d2b97ed98d9e598dc017bdff88f0927838
hyrumnoble <hyrumnoble3@gmail.com>
2023-05-13 07:52:39 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate a random number between 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        // Ask the user to guess the number
+        Console.Write("Guess the magic number between 1 and 100: ");
+        int guess = int.Parse(Console.ReadLine());
+
+        // Keep looping until the user guesses correctly
+        while (guess != magicNumber)
+        {
+            // Provide a hint to the user about whether their guess was too high or too low
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            // Ask the user to guess again
+            Console.Write("Guess again: ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        // The user guessed correctly!
+        Console.WriteLine("You guessed it! The magic number was " + magicNumber);
     }
 }
